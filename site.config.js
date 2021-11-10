c20b300e6f076a1b073022203a6719fbcdbbc1ff
Dakki <yewzheexiu@gmail.com>
2021-11-11 00:21:56 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,25 +1,25 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'u84ecu83b1-ea9341de51274eeba6f52a27916f961e',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '仙人界',
+  domain: 'u84ecu83b1-ea9341de51274eeba6f52a27916f961e',
+  author: 'Dakki',
 
   // open graph metadata (optional)
-  description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
-  socialImageSubtitle: 'Hello World! 👋',
+  description: '神秘漂浮岛',
+  socialImageTitle: '女狐',
+  socialImageSubtitle: '妲己姐姐的烹饪教学事件！',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'dakki_so',
+  github: 'venusyew',
+  linkedin: '',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
